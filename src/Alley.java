d8bbec1b925e61859be177a85898bb883b4f54f1
Salvador Jacobi <salvador.jacobi@gmail.com>
2013-11-07 15:38:53 +0100

    alley monitor solution
    This was much easier to do than with semaphores.

--- src/Alley.java
+++ src/Alley.java
@@ -1,41 +1,24 @@
 public class Alley {
-	private Semaphore mutex = new Semaphore(1);
-	private Semaphore top = new Semaphore(0);
-	private Semaphore bot = new Semaphore(0);
-
 	private int n = 0; // number of cars in alley
-	private int w = 0; // number of cars waiting
 	private boolean down = true; // last observed travel direction
 
 	private boolean goesDown(int no) {
 		return no < 5;
 	}
 
-	public void enter(int no) {
+	public synchronized void enter(int no) {
 		boolean goesDown = goesDown(no);
 
-		while (true) {
-			try { mutex.P(); } catch (InterruptedException e) {}
-			if (n == 0 || goesDown == down) break;
-			w++;
-			mutex.V();
-			try { (goesDown ? top : bot).P(); } catch (InterruptedException e) {}
+		if (n > 0 && goesDown != down) {
+		    try { wait(); } catch (InterruptedException e) {}
 		}
+
 		n++;
 		if (n == 1) down = goesDown;
-		mutex.V();
 	}
 
-	public void leave(int no) {
-		try { mutex.P(); } catch (InterruptedException e) {}
-		n--;
-		if (n == 0) {
-			Semaphore s = goesDown(no) ? bot : top;
-			while (w > 0) {
-				w--;
-				s.V();
-			}
-		}
-		mutex.V();
+	public synchronized void leave(int no) {
+	    n--;
+	    if (n == 0) notifyAll();
 	}
 }
