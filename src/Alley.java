da3246642c218ba3cb33d20a5c12ac5d7a091fda
Salvador Jacobi <salvador.jacobi@gmail.com>
2013-11-14 18:29:35 +0100

    attempt at fair alley monitor (can deadlock!)

--- src/Alley.java
+++ src/Alley.java
@@ -1,20 +1,29 @@
 public class Alley {
 	private int n = 0; // number of cars in alley
+	private int wt = 0; // number of cars waiting at top
+	private int wb = 0; // number of cars waiting at bottom
 	private boolean down = true; // last observed travel direction
 
 	private boolean goesDown(int no) {
 		return no < 5;
 	}
 
+	// TODO: Fix deadlock...
+
 	public synchronized void enter(int no) throws InterruptedException {
 		boolean goesDown = goesDown(no);
 
-		if (n > 0 && goesDown != down) {
+		if (goesDown) wt++; else wb++;
+
+		while ((n > 0 && goesDown != down) || (goesDown ? wb > 0 : wt > 0)) {
 		    wait();
+		    if (down != goesDown) break;
 		}
 
+		if (goesDown) wt--; else wb--;
+
 		n++;
-		if (n == 1) down = goesDown;
+		down = goesDown;
 	}
 
 	public synchronized void leave(int no) {
