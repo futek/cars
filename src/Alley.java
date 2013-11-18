ca843371bbb54992ea22273f8ceb7a1c73f119b8
Salvador Jacobi <salvador.jacobi@gmail.com>
2013-11-18 22:52:33 +0100

    fix indentation (only spaces!)

--- src/Alley.java
+++ src/Alley.java
@@ -1,34 +1,34 @@
 public class Alley {
-	private int n = 0; // number of cars in alley
-	private int wt = 0; // number of cars waiting at top
-	private int wb = 0; // number of cars waiting at bottom
-	private boolean down = true; // last observed travel direction
+  private int n = 0; // number of cars in alley
+  private int wt = 0; // number of cars waiting at top
+  private int wb = 0; // number of cars waiting at bottom
+  private boolean down = true; // last observed travel direction
 
-	private boolean goesDown(int no) {
-		return no < 5;
-	}
+  private boolean goesDown(int no) {
+    return no < 5;
+  }
 
-	private boolean allowedToEnter(boolean goesDown) {
-        boolean emptyAndNewDirection = n == 0 && down != goesDown;
-        boolean emptyOrSameDirection = n == 0 || goesDown == down;
-        boolean noCarsWaiting = goesDown && wb == 0 || !goesDown && wt == 0;
+  private boolean allowedToEnter(boolean goesDown) {
+    boolean emptyAndNewDirection = n == 0 && down != goesDown;
+    boolean emptyOrSameDirection = n == 0 || goesDown == down;
+    boolean noCarsWaiting = goesDown && wb == 0 || !goesDown && wt == 0;
 
-        return emptyAndNewDirection || emptyOrSameDirection && noCarsWaiting;
-    }
+    return emptyAndNewDirection || emptyOrSameDirection && noCarsWaiting;
+  }
 
-	public synchronized void enter(int no) throws InterruptedException {
-		boolean goesDown = goesDown(no);
+  public synchronized void enter(int no) throws InterruptedException {
+    boolean goesDown = goesDown(no);
 
-		if (goesDown) wt++; else wb++;
-		while (!allowedToEnter(goesDown)) wait();
-		if (goesDown) wt--; else wb--;
+    if (goesDown) wt++; else wb++;
+    while (!allowedToEnter(goesDown)) wait();
+    if (goesDown) wt--; else wb--;
 
-		n++;
-		down = goesDown;
-	}
+    n++;
+    down = goesDown;
+  }
 
-	public synchronized void leave(int no) {
-	    n--;
-	    if (n == 0) notifyAll();
-	}
+  public synchronized void leave(int no) {
+    n--;
+    if (n == 0) notifyAll();
+  }
 }
